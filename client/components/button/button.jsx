c57dcee548d6cbb84b861ec64f03443a002d372f
Romain DEJEAN <romain.dejean@gadz.org>
2017-03-27 08:56:16 -0700

    Setup button component but basic class

--- client/components/button/button.jsx
+++ client/components/button/button.jsx
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 
 
 export default class Button extends Component { 
+
+  handleClick = e => {
+    const { clickHandler } = this.props;
+    clickHandler(e);
+  }
+
   render() {
-    const { value } = this.props;
+    const { value, styleClassName, containerStyleClassName } = this.props;
+    let buttonContainerClass = 'button-container ' + containerStyleClassName;
+    let buttonClass = 'button ' + styleClassName;
+    
     return (
-      <div>
-          <button>{value}</button>
+      <div className={buttonContainerClass}>
+          <button className={buttonClass} onClick={this.handleClick}>{value}</button>
       </div>
     );
   }
