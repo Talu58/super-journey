f2404abb4270323a2d7b90e17365a699e674ec8c
Romain DEJEAN <romain.dejean@gadz.org>
2017-03-30 09:18:02 -0700

    Added NPO project description step

--- client/views/signup-steps/signup-steps.jsx
+++ client/views/signup-steps/signup-steps.jsx
@@ -11,7 +11,8 @@ export default class SignUpSteps extends Component {
       step: 1,
       role: { Donor: false, 'Non-Profit Organisation': false },
       industry: {Healthcare: false, Tech: false, Climat: false, Inclusion: false, 'Global Change': false},
-      hasRole: false
+      hasRole: false,
+      hasChoosenOption: false
     }
     this.clickOptionHandler = this.clickOptionHandler.bind(this);
     this.clickPrevHandler = this.clickPrevHandler.bind(this);
@@ -28,7 +29,8 @@ export default class SignUpSteps extends Component {
       };
       this.setState({
         role: newState,
-        hasRole: false
+        hasRole: false,
+        hasChoosenOption: false
       });
     } else if (this.state.step === 1) {
       console.log('clickOptionHandler step1');
@@ -39,7 +41,8 @@ export default class SignUpSteps extends Component {
       };
       this.setState({
         role: newState,
-        hasRole: true
+        hasRole: true,
+        hasChoosenOption: true
       });
     } else {
       console.log('clickOptionHandler step2');
@@ -50,7 +53,8 @@ export default class SignUpSteps extends Component {
       };
       this.setState({
         industry: newState,
-        hasRole: true
+        hasRole: true,
+        hasChoosenOption: true
       });
     }
   }
@@ -65,7 +69,8 @@ export default class SignUpSteps extends Component {
   clickNextHandler() {
     const { step } = this.state;
     this.setState({
-      step: step+1
+      step: step+1,
+      hasChoosenOption: false
     });
   }
 
@@ -75,14 +80,28 @@ export default class SignUpSteps extends Component {
       case 1:
         display = (
           <div>
-            <SignUpStep disabled={this.state.hasRole} buttons={this.state.role} clickHandler={this.clickOptionHandler}/>
+            <SignUpStep
+              disabled={this.state.hasRole}
+              buttons={this.state.role}
+              clickHandler={this.clickOptionHandler}
+            />
           </div>
         );
         break;
       case 2:
         display = (
           <div>
-            <SignUpStep buttons={this.state.industry} clickHandler={this.clickOptionHandler}/>
+            <SignUpStep
+              buttons={this.state.industry}
+              clickHandler={this.clickOptionHandler}
+            />
+          </div>
+        );
+        break;
+      case 3:
+        display = (
+          <div>
+            Enter title and description
           </div>
         );
         break;
@@ -98,11 +117,33 @@ export default class SignUpSteps extends Component {
         {display}
         </div>
         <div className="signup-navigation-button-container">
-          {this.state.step === 1 ? <Button disabled={true} styleClassName="button-primary" value="Prev"/>
-            : <Button clickHandler={this.clickPrevHandler} value="Prev" styleClassName="button-primary"/>
+          {this.state.step === 1 ? 
+            <Button
+              disabled={true}
+              styleClassName="button-primary"
+              value="Prev"
+            />
+            : 
+            <Button
+              clickHandler={this.clickPrevHandler}
+              value="Prev"
+              styleClassName="button-primary"
+            />
           }
-          {this.state.step !== 2 ? <Button clickHandler={this.clickNextHandler} styleClassName="button-primary" value="Next"/>
-            : <Button clickHandler={this.clickSaveHandler} value="Save" styleClassName="button-primary"/>
+          {(this.state.step === 2 && this.state.role.Donor) || this.state.step === 3 ? 
+            <Button 
+              disabled={!this.state.hasChoosenOption}
+              clickHandler={this.clickSaveHandler}
+              value="Save"
+              styleClassName="button-primary"
+            />
+            : 
+            <Button
+              disabled={!this.state.hasChoosenOption}
+              clickHandler={this.clickNextHandler}
+              styleClassName="button-primary"
+              value="Next"
+            />
           }
         </div>
       </div>
