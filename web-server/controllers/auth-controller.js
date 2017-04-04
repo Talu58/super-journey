d7f3fa370696ea9a0ae3fc967ec8cdc06f9c9bc1
Romain DEJEAN <romain.dejean@gadz.org>
2017-04-04 14:13:40 -0700

    Setup User Uniqueness on signup

--- web-server/controllers/auth-controller.js
+++ web-server/controllers/auth-controller.js
@@ -1,29 +1,53 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-const { User, Role, Industry } = require('../../database/user-model');
+const { User } = require('../../database/user-model');
 const { createRole, createIndustry } = require('../utils/utils-profile');
 const { generateHashedPassword, compareHashedPassword } = require('../utils/utils-auth');
 
 module.exports.signUpRequest = (req, res) => {
   const { email, password } = req.body;
-  const newUser = new User;
-  newUser.email = email;
-  generateHashedPassword(password).then(hash => {
-    newUser.password = hash;
-  }).then( () => {
-    return newUser.save((err, user) => {
-      if (err) {
+  User.findOne({ 'email': email }, (err, user) => {
+    if (err) {
+      throw err;
+    } else if (user) {
+      compareHashedPassword(password, user.password).then(isValid => {
+        if (isValid) {
+          return res.send({
+            email: email,
+            completedProfile: user.completedProfile,
+          });
+        } else {
+          return res.status(401).send('Password Incorrect');
+        }
+      });
+    } else {
+      const newUser = new User;
+      newUser.email = email;
+      generateHashedPassword(password).then(hash => {
+        newUser.password = hash;
+      }).then( () => {
+        return newUser.save((err, user) => {
+          if (err) {
+            throw err;
+          } else {
+            return res.send({
+              email: email,
+              completedProfile: user.completedProfile
+            });
+          }
+        })
+      }).catch(err => {
         throw err;
-      } else {
-        return res.send({
-          email: email,
-          completedProfile: user.completedProfile
-        });
-      }
-    })
-  }).catch(err => {
-    throw err;
-  });  
+      });  
+    }
+  })
+
+
+
+
+
+
+  
 };
 
 module.exports.signUpCompletedRequest = (req, res) => {
