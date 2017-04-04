0695e12e28a4b84ba2de68483ef8792e5ae60659
Romain DEJEAN <romain.dejean@gadz.org>
2017-04-04 14:30:02 -0700

    Setup donor and non profit components

---
+++ client/views/home/donor/donor.jsx
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react';
+import './_donor.sass';
+
+class Donor extends Component {
+  render() {
+    return (
+      <div>
+        Donor
+      </div>
+    );
+  }
+}
+
+Donor.propTypes = {
+
+};
+
+export default Donor;
---
+++ client/views/home/non-profit/non-profit.jsx
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react';
+import './_non-profit.sass';
+
+class NonProfit extends Component {
+  render() {
+    return (
+      <div>
+        NonProfit
+      </div>
+    );
+  }
+}
+
+NonProfit.propTypes = {
+
+};
+
+export default NonProfit;
