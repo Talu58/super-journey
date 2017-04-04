7260cbdae69c73357fc5d5807ed8bae222c3e2cb
Romain DEJEAN <romain.dejean@gadz.org>
2017-04-04 14:23:29 -0700

    Setup new project request

--- web-server/utils/utils-auth.js
+++ web-server/utils/utils-auth.js
@@ -3,7 +3,6 @@ const saltRounds = 10;
 
 module.exports.generateHashedPassword = password => {
   return bcrypt.hash(password, saltRounds).then(hash => {
-        console.log('generateHashedPassword', hash);
     return hash;
   }).catch(err => {
     console.log('generateHashedPassword', err);
