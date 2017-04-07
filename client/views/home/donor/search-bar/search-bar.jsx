647e2873e79621a4d3a7414b866161c848191829
Romain DEJEAN <romain.dejean@gadz.org>
2017-04-07 16:03:40 -0700

    Setup basic search bar style

--- client/views/home/donor/search-bar/search-bar.jsx
+++ client/views/home/donor/search-bar/search-bar.jsx
@@ -1,22 +1,27 @@
 import React, { Component, PropTypes } from 'react';
 import './_search-bar.sass';
 import CheckBox from '../../../../components/checkbox/checkbox';
+import InputField from '../../../../components/input-field/input-field';
+import Button from '../../../../components/button/button';
+
 
 class SearchBar extends Component {
   render() {
     const { industries } = this.props;
     return (
-      <div>
-        {industries.map(industry => {
-          return <CheckBox key={industry} label={industry} />
-        })}
+      <div className="search-bar-container">
+        <div className="search-container">
+          <InputField placeholderText="Search"/>
+          <Button value="Search"/>
+        </div>
+        <div className="checkboxes-container">
+          {industries.map(industry => {
+            return <CheckBox key={industry} label={industry} />
+          })}
+        </div>
       </div>
     );
   }
 }
-
-SearchBar.propTypes = {
-
-};
 
 export default SearchBar;
