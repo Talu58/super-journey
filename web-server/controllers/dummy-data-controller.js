cfd17159cc38f1443fcb5c869cb676bbed9bc21b
Romain DEJEAN <romain.dejean@gadz.org>
2017-04-10 18:09:21 -0700

    Setup randomly generated data

--- web-server/controllers/dummy-data-controller.js
+++ web-server/controllers/dummy-data-controller.js
@@ -1,12 +1,36 @@
-const { 
-  signUpRequest,
-  signUpCompletedRequest,
-} = require('./auth-controller');
-const {
-  generateDummyData
-} = require('../../database/demo-data');
+const { signUpRequest, signUpCompletedRequest } = require('./auth-controller');
+const { User } = require('../../database/user-model');
+const { createRole, createIndustry, createProject } = require('../utils/utils-profile');
+const { generateHashedPassword } = require('../utils/utils-auth');
+const { generateDummyData } = require('../../database/demo-data');
 
 module.exports.createDummyUsers = (req, res) => {
   const dummyData = generateDummyData();
+  dummyData.forEach(user => {
+    const { email, password, industry, role, project } = user;
+    const newUser = new User;
+    newUser.email = email;
+
+    let newRole = createRole(role);
+    let newIndustry = createIndustry(industry);
+    let newProject = createProject(project);
+
+    newUser.completedProfile = true;
+    newUser.role = newRole;
+    newUser.industry = newIndustry;
+    newUser.project = newProject;
+
+    generateHashedPassword(password).then(hash => {
+      newUser.password = hash;
+    }).then( () => {
+      return newUser.save((err, user) => {
+        if (err) {
+          throw err;
+        } 
+      })
+    }).catch(err => {
+      throw err;
+    });
+  });
   res.send({});
 };
