301654cb151efb5eeb979c7bcf604d539098cc4a
Romain DEJEAN <romain.dejean@gadz.org>
2017-04-12 23:50:29 -0600

    Setup socket.io

--- web-server/server.js
+++ web-server/server.js
@@ -3,8 +3,9 @@ const path = require('path');
 const bodyParser = require ('body-parser');
 const routes = require('./routes');
 
-
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const db = require('../database/db-config.js');
 
 const PORT = process.env.PORT || 3000;
@@ -15,6 +16,10 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+});
+
+io.on('connection', function(socket) {  
+  console.log('A user connected');
 });
 
 app.listen(PORT, () => {
