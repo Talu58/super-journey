2da61e9df470e9872b9c7b38479d460032c0e583
Romain DEJEAN <romain.dejean@gadz.org>
2017-04-18 16:06:50 -0400

    Setup inputfield and button to send new message

--- client/views/messaging/message-thread/message-thread.jsx
+++ client/views/messaging/message-thread/message-thread.jsx
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 import './_message-thread.sass';
 import Message from '../../../components/message/message';
+import Button from '../../../components/button/button';
+import InputField from '../../../components/input-field/input-field';
 
 class MessageThread extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messageInputFieldValue: ''
+    }
+    this.newMessageInputFieldChange = this.newMessageInputFieldChange.bind(this);
+    this.sendNewMessageClicked=this.sendNewMessageClicked.bind(this);
+  }
+
+  newMessageInputFieldChange(e) {
+    let newMessageInputFieldValue = e.target.value;
+    this.setState({
+      messageInputFieldValue: newMessageInputFieldValue
+    });
+  }
+
+  sendNewMessageClicked() {
+    if (this.state.messageInputFieldValue !== '') {
+      console.log('send message', this.state.messageInputFieldValue);
+      this.setState({
+        messageInputFieldValue: ''
+      });
+    } else {
+      console.log('nothing');
+    }
+  }
+
   render() {
     const { currentMessageThread, currentMessageThreadUserName, curentUserFirstName } = this.props;
 
@@ -18,6 +47,18 @@ class MessageThread extends Component {
             />
           );
         })}
+        <div>
+          <InputField
+            placeholderText="Enter your message here"
+            changeHandler={this.newMessageInputFieldChange}
+            value={this.state.messageInputFieldValue}
+          />
+          <Button
+            value="Send"
+            styleClassName="button-primary"
+            clickHandler={this.sendNewMessageClicked}
+          />
+        </div>
       </div>
     );
   }
