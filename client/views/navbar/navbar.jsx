b209ea545b69a4b493cf53f2c37d57d7d225dd81
Romain DEJEAN <romain.dejean@gadz.org>
2017-04-20 09:58:40 -0700

    fixed navbar state on refresh

--- client/views/navbar/navbar.jsx
+++ client/views/navbar/navbar.jsx
@@ -1,21 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 import Button from '../../components/button/button';
 import LoginForm from './login-form/login-form';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './_navbar.sass';
 
 export default class NavBar extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      nav: {
-        home: true,
-        messaging: false,
-        profile: false
-      }
-    }
+
     this.logout = this.logout.bind(this);
-    this.navButtonClicked = this.navButtonClicked.bind(this);
   }
 
   logout() {
@@ -23,62 +16,33 @@ export default class NavBar extends Component {
     logout();
   }
 
-  navButtonClicked(e) {
-    let newNavState = {};
-    for (let key in this.state.nav) {
-      if (key === e.target.name) {
-        newNavState[key] = true
-      } else {
-        newNavState[key] = false
-      }
-    }
-    this.setState({
-      nav: newNavState
-    });
-  }
-
   render() {
     const { isAuth, completedProfile } = this.props;
     return (
       <header className="navbar-container">
         <div className="logo-container"> 
-          <Link to="/">
+          <NavLink to="/">
             <img src="http://localhost:3000/assets/logo.png" className="nav-logo"/>
-          </Link>
+          </NavLink>
         </div>
         <div className="navbar-actions-container">
           {isAuth && completedProfile ? 
             <div className="navbar-actions-nav-container" >
-              <Link to="/home" className="nav-link" >
-                <Button
-                  name="home"
-                  value="HOME"
-                  containerStyleClassName="nav-button-container"
-                  styleClassName="nav-button"
-                  active={this.state.nav.home}
-                  clickHandler={this.navButtonClicked}
-                />
-              </Link>
-              <Link to="/messaging" className="nav-link" >
-                <Button
-                  name="messaging"
-                  value="MESSAGING"
-                  containerStyleClassName="nav-button-container"
-                  styleClassName="nav-button"
-                  active={this.state.nav.messaging}
-                  clickHandler={this.navButtonClicked}
-                />
-              </Link>
-              <Link to="/profile" className="nav-link" >
-                <Button
-                  name="profile"
-                  value="PROFILE"
-                  containerStyleClassName="nav-button-container"
-                  styleClassName="nav-button"
-                  active={this.state.nav.profile}
-                  clickHandler={this.navButtonClicked}
-                />
-              </Link>
+              <NavLink
+                to="/home"
+                className="nav-link"
+                activeClassName="nav-link-active"
+              >HOME</NavLink>
+              <NavLink
+                to="/messaging"
+                className="nav-link"
+                activeClassName="nav-link-active"
+              >MESSAGING</NavLink>
+              <NavLink
+                to="/profile"
+                className="nav-link"
+                activeClassName="nav-link-active"
+              >PROFILE</NavLink>
             </div>
             : null
           }
