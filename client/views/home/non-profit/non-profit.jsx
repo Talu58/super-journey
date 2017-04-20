523b8cf310eeccfd9400f07f1c8f55fa32d1e2c0
Romain DEJEAN <romain.dejean@gadz.org>
2017-04-20 11:29:53 -0700

    Setup organization profile page

--- client/views/home/non-profit/non-profit.jsx
+++ client/views/home/non-profit/non-profit.jsx
@@ -1,13 +1,26 @@
 import React, { Component, PropTypes } from 'react';
 import './_non-profit.sass';
+import Button from '../../../components/button/button';
+
 
 class NonProfit extends Component {
   render() {
     const { organization } = this.props;
-    console.log('NonProfit organization', organization);
     return (
-      <div>
-        NonProfit
+      <div className="organization-profile-view">
+        <div className="organization-profile-container">
+          <h1 className="organization-profile-information-header">Your Organization's information:</h1>
+          <section className="organization-profile-information-container">
+            <p>Name:</p>
+            <p className="organization-details">{organization.name}</p>
+            <p>Description:</p>
+            <p className="organization-details">{organization.description}</p>
+          </section>
+          <Button
+            value="Edit"
+            styleClassName="organization-profile-edit-button"
+          />
+        </div>
       </div>
     );
   }
