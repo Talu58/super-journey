cccf34893a8fea190e10db94441e3063881a523f
Romain DEJEAN <romain.dejean@gadz.org>
2017-04-21 13:53:40 -0700

    Added header to landing page

--- client/views/landing-page/landing-page.jsx
+++ client/views/landing-page/landing-page.jsx
@@ -4,9 +4,15 @@ import SignUpForm from './signupform/signupform';
 import TryDemo from './try-demo/try-demo';
 
 const LandingPage = () => (
-  <div className="landing-page-container" >
-    <TryDemo/>
-    <SignUpForm/>
+  <div className="landing-page-view-container" >
+    <div className="landing-page-header-container">
+      <h1 className="landing-page-header">Welcome to Ignite Impact!</h1>
+      <p className="landing-page-header-description">We match donors and non profit organizations to facilitate the discussions and boost the impacts</p>
+    </div>
+    <section className="landing-page-content-container" >
+      <TryDemo/>
+      <SignUpForm/>
+    </section>
   </div>
 );
 
