7d3a3adaba060f92f2530d5d2fd0827a5822e29f
Romain DEJEAN <romain.dejean@gadz.org>
2017-04-22 10:32:22 -0700

    Connected DB to mLab

--- database/db-config.js
+++ database/db-config.js
@@ -1,11 +1,23 @@
 const mongoose = require('mongoose');
+require('dotenv-safe').load();
 
-mongoose.connect('mongodb://localhost/users');
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };       
+var mongodbUri = process.env.MONGO_DB_URI;
+
+//Comment the following line to run on local host
+mongoose.connect(mongodbUri, options);
+ 
+// Un-comment the following line to run on local host
+// mongoose.connect('mongodb://localhost/users');
+
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error'));
+db.on('error', console.error.bind(console, 'connection error:')); 
 db.once('open', () => {
-  //uncomment the following line to drop the DB:
+  //Un-comment the following line to drop the DB:
   // db.dropDatabase();
   console.log('db connected with mongoose');
-})
+});
+
+
